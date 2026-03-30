697a841b48a76a418825793549b9e84ec82f2f88
enzomkt-debug <enzomkt@gmail.com>
2026-03-29 22:00:03 -0300

    fix: monitor a cada 5min, educativo 9h e 15h

--- index.js
+++ index.js
@@ -769,8 +769,9 @@ cron.schedule('0 12 * * *',       function() { runContexto() },         { timezo
 cron.schedule('0 13 * * *',       function() { runAgent('tarde') },     { timezone: 'America/Sao_Paulo' })
 cron.schedule('0 19 * * *',       function() { runAgent('noite') },     { timezone: 'America/Sao_Paulo' })
 cron.schedule('0 9 * * *',        function() { runEducativo() },        { timezone: 'America/Sao_Paulo' })
-cron.schedule('*/30 14-23 * * *', function() { monitorarResultados() }, { timezone: 'America/Sao_Paulo' })
-cron.schedule('*/30 0-2 * * *',  function() { monitorarResultados() }, { timezone: 'America/Sao_Paulo' })
+cron.schedule('0 15 * * *',       function() { runEducativo() },        { timezone: 'America/Sao_Paulo' })
+cron.schedule('*/5 14-23 * * *', function() { monitorarResultados() }, { timezone: 'America/Sao_Paulo' })
+cron.schedule('*/5 0-2 * * *',   function() { monitorarResultados() }, { timezone: 'America/Sao_Paulo' })
 
 console.log('Agente agendado: 8h, 12h, 13h, 15h e 19h (horario de Brasilia)')
-console.log('Monitor de resultados: a cada 30 minutos entre 14h e 02h')
+console.log('Monitor de resultados: a cada 5 minutos entre 14h e 02h')
